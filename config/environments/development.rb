7d9f00c4ed8cb45d8d70934ca18bfd53d3515653
Jordan Benning <jbenning97@gmail.com>
2020-12-08 05:36:57 +0000

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,12 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  
+  host = 'https://4c9cf75ced9e44a989bac5f21fc8723e.vfs.cloud9.us-east-2.amazonaws.com' # Don't use this literally; use your local dev host instead
+  # Use this on the cloud IDE.
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  # Use this if developing on localhost.
+  # config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
   config.action_mailer.perform_caching = false
 
